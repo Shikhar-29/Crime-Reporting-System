08118e817cab0b07ff2c8ec4ee92621fbead5cb4
Shikhar Saxena <shikhar.saxena2015@vit.ac.in>
2017-09-28 23:18:51 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<div class="footer clear">
+    	<span>Crime Reporting © 2017  |  <a href="#">Privacy Policy</a></span> Crime Reporting is for registering FIR any time and any wherever released. &nbsp;&nbsp;&nbsp;<!--{%FOOTER_LINK} -->
+    </div>
+</div>
+<!--osc3.template-help.com -->
+
+
+</body></html>
